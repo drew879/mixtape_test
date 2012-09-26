abb6115ba1aab479e5111be2a6e76e37fd9cbb1a
Drew McCalmont <drew879@gmail.com>
2012-09-25 21:22:40 -0400

    Reinstituted authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # before_filter :login_required
+  before_filter :login_required
 
-  # private
+  private
   
-  # def login_required
-  #   redirect_to login_path, :notice => "You must be logged in to access #{request.path}." unless logged_in?
-  # end
+  def login_required
+    redirect_to login_path, :notice => "You must be logged in to access #{request.path}." unless logged_in?
+  end
 
-  # def logged_in?
-  #   @user = User.find(session[:user_id]) if session[:user_id]
-  # end
+  def logged_in?
+    @user = User.find(session[:user_id]) if session[:user_id]
+  end
 end
